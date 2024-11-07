c60a971d748271dd8cee371930ad508481ec767f
saviido <yluo0801@uni.sydney.edu.au>
2024-11-08 05:06:03 +1100

    Second attempt
    1. Modified the swing angle of the lower wing so that the two wings swing in opposite directions. 2. Modified some coordinate points to reduce the intersection of lines and move the tail slightly downward.

--- p5_project/sketch.js
+++ p5_project/sketch.js
@@ -8,11 +8,11 @@ function setup() {
 
 function draw() {
   background(251, 244, 236);// Redraw the background
-// Set the swing angle
-  let angle1 = sin(frameCount * 0.05) * 10; // The swing angle of the upper wing
-  let angle2 = cos(frameCount * 0.05) * 10; // The swing angle of the lower wing
+  // Set the swing angle
+  let angle1 = sin(frameCount * 0.05) * 20;  // The swing angle of the upper wing
+  let angle2 = 40 - sin(frameCount * 0.05) * 20; // The swing angle of the lower wing
 
- // pigeon's head and chest
+  // pigeon's head and chest
   noFill();
   stroke(42, 48, 72);
   strokeWeight(2);
@@ -20,12 +20,12 @@ function draw() {
   bezier(370, 120, 392, 83, 429, 98, 440, 100);
   bezier(440, 100, 458, 99, 470, 110, 480, 109);
   bezier(480, 109, 447, 118, 440, 159, 440, 180);
-  bezier(440, 180, 440, 280, 372, 328, 340, 340);  
+  bezier(440, 180, 440, 280, 372, 340, 410, 300);  
 
   // the wing above
   stroke(42, 48, 72);
   strokeWeight(2);
-  bezier(20 + angle1, 40 + angle1, 160 + angle1, 18 + angle1, 323 + angle1, 116 + angle1, 372, 172);
+  bezier(20 + angle1, 40 + angle1, 160 + angle1, 18 + angle1, 323 + angle1, 116 + angle1, 340, 150);
   bezier(20 + angle1, 40 + angle1, 20 + angle1, 60 + angle1, 70 + angle1, 69 + angle1, 83 + angle1, 80 + angle1);
   bezier(83 + angle1, 80 + angle1, 0 + angle1, 98 + angle1, 28 + angle1, 108 + angle1, 100 + angle1, 120 + angle1);
   bezier(100 + angle1, 120 + angle1, 18 + angle1, 141 + angle1, 3 + angle1, 160 + angle1, 80 + angle1, 160 + angle1);
@@ -39,14 +39,14 @@ function draw() {
   bezier(192 + angle2, 348 + angle2, 125 + angle2, 438 + angle2, 138 + angle2, 442 + angle2, 230 + angle2, 370 + angle2);
   bezier(230 + angle2, 370 + angle2, 145 + angle2, 448 + angle2, 190 + angle2, 442 + angle2, 220 + angle2, 425 + angle2);
   bezier(220 + angle2, 425 + angle2, 330 + angle2, 371 + angle2, 383 + angle2, 298 + angle2, 330 + angle2, 220 + angle2);
-  bezier(330 + angle2, 220 + angle2, 318 + angle2, 202, 281, 208, 300, 230);
+  bezier(330 + angle2, 220 + angle2, 318 + angle2, 202 + angle2, 281 + angle2, 208 + angle2, 300, 230);
 
   // tail
-  bezier(230, 180, 209, 256, 20, 283, 7, 263);
-  bezier(7, 263, 6, 303, 24, 322, 60, 296);
-  bezier(60, 296, 24, 322, 10, 376, 100, 315);
-  bezier(100, 315, 43, 369, 51, 408, 103, 343);
-  bezier(103, 343, 82, 398, 95, 435, 176, 314);
+  bezier(230 + 30, 180 + 30, 209 + 30, 256 + 30, 20 + 30, 283 + 30, 7 + 30, 263 + 30);
+  bezier(7 + 30, 263 + 30, 6 + 30, 303 + 30, 24 + 30, 322 + 30, 60 + 30, 296 + 30);
+  bezier(60 + 30, 296 + 30, 24 + 30, 322 + 30, 10 + 30, 376 + 30, 100 + 30, 315 + 30);
+  bezier(100 + 30, 315 + 30, 43 + 30, 369 + 30, 51 + 30, 408 + 30, 103 + 30, 343 + 30);
+  bezier(103 + 30, 343 + 30, 82 + 30, 398 + 30, 95 + 30, 435 + 30, 176 + 30, 314 + 30);
 
   //leaf
   fill(26, 119, 75);
@@ -57,7 +57,7 @@ function draw() {
   bezier(500, 40, 458, 80, 470, 100, 500, 40);
   bezier(497, 70, 443, 91, 485, 94, 498, 70);
 
- //leaves branch
+  //leaves branch
   noFill();
   stroke(26, 119, 75);
   strokeWeight(1.5);
@@ -78,4 +78,3 @@ function draw() {
   noFill();
   ellipse(425, 112, 12, 12);
 }
-
